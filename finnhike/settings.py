0129cd4da794a87f162b2d58273cf4a614ff42ef
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2024-09-21 22:25:23 +0300

    updated markup, places views

--- finnhike/settings.py
+++ finnhike/settings.py
@@ -41,8 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home',
-    'places',
+    'home.apps.HomeConfig',
+    'places.apps.PlacesConfig',
 ]
 
 MIDDLEWARE = [
