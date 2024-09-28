6a80e21984dba85304789c0e0bdc2bf8d6773706
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2024-09-28 21:55:13 +0300

    added icons, added class for getting current weather, modified function based to class based getting and saving data from DataHub, updated place mark up

--- places/services.py
+++ places/services.py
@@ -1,8 +1,167 @@
+import os
+import json
+import requests
+
+from decimal import Decimal
 from collections import defaultdict
 
+from dotenv import load_dotenv
+
+from django.http import JsonResponse
 from django.db.models import Q
 
 from places.models import Place
+
+load_dotenv()
+
+
+class DataHubAPI:
+    @staticmethod
+    def get_api_data_for_datahub():
+        url = 'https://iam-datahub.visitfinland.com/auth/realms/Datahub/protocol/openid-connect/token'
+
+        access_data = {
+            'client_id': 'datahub-api',
+            'client_secret': f'{os.getenv("SECRET_KEY")}',
+            'grant_type': 'password',
+            'username': f'{os.getenv("USERNAME")}',
+            'password': f'{os.getenv("DH_PASSWORD")}'
+        }
+
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+        return url, access_data, headers
+
+    def get_access(self):
+        url, access_data, headers = self.get_api_data_for_datahub()
+        try:
+            response = requests.post(url, data=access_data, headers=headers)
+            access_token = response.json().get('access_token')
+            return access_token
+        except requests.exceptions.RequestException as e:
+            return {"error": str(e)}
+
+    def get_api_data_for_graphql_query(self, query):
+        request_url = 'https://api-datahub.visitfinland.com/graphql/v1/graphql'
+        access_token = self.get_access()
+
+        headers = {
+            'Authorization': f'Bearer {access_token}',
+            'Content-Type': 'application/json'
+        }
+
+        body = {
+            'query': query,
+        }
+        return request_url, headers, body
+
+    @staticmethod
+    def get_query():
+        query = """ {
+                    product (where: {
+                        _and: [
+                            {type:{_eq:attraction}},
+                            {productTags:{tag:{_eq:"hiking_walking_trekking"}}},
+                            {productTargetGroups:{targetGroupId:{_eq:b2c}}},
+                        ]}){
+                        productImages {
+                            filename
+                            altText
+                            largeUrl
+                        }
+                        productInformations {
+                            description
+                            name
+                            url
+                        }
+                        postalAddresses {
+                            location
+                            postalCode
+                            streetName
+                            city
+                        }
+                        productAvailableMonths{
+                            month
+                        }
+                        productPricings {
+                            fromPrice
+                            toPrice
+                        }
+                    }
+                }"""
+        return query
+
+    def make_api_request(self):
+        request_url, headers, body = self.get_api_data_for_graphql_query(self.get_query())
+        try:
+            response = requests.post(request_url, headers=headers, data=json.dumps(body))
+            response_data = response.json()
+            return response_data
+        except Exception as e:
+            return JsonResponse(f"Error getting access token: {e}", status=400)
+
+    def save_data(self, filename):
+        with open(filename, 'w') as f:
+            data = self.make_api_request()
+            json.dump(data, f)
+
+
+# test = DataHubAPI()
+# test.save_data('test.json')
+
+
+# clear all data by searching for english and finnish data and deleting another languages data
+class DataFromDataHub:
+    def get_data_from_file(self, filename):
+        with open(filename, 'r') as f:
+            data = json.load(f)
+            for product in data['product']:
+                # image info
+                image_path = product['productImages'][0]['filename']
+                image_alt_text = product['productImages'][0]['altText'].strip() if product['productImages'][0][
+                                                                                       'altText'] is not None else 'Product image'
+                image_url = product['productImages'][0]['largeUrl']
+
+                # product info in english
+                description_eng = product['productInformations'][0]['description']
+                name_eng = product['productInformations'][0]['name']
+                url = product['productInformations'][0]['url'] if product['productInformations'][0]['url'] else \
+                    product['productInformations'][1]['url']
+
+                # product info in finnish
+                description_fin = product['productInformations'][1]['description']
+                name_fin = product['productInformations'][1]['name']
+
+                # location info
+                location = product['postalAddresses'][0]['location'][1:-1].split(',')
+                latitude = Decimal(location[0])
+                longitude = Decimal(location[1])
+                postal_code = product['postalAddresses'][0]['postalCode']
+                street_name = product['postalAddresses'][0]['streetName']
+                city = product['postalAddresses'][0]['city']
+
+                # time for visiting info
+                available_time = ', '.join([month['month'] for month in product['productAvailableMonths']])
+
+                # price info
+                price = 'Free' if (product['productPricings'][0]['fromPrice'] == 0.0 and product['productPricings'][0][
+                    'toPrice'] == 0.0) else f'Paid'
+
+                self.insert_data_into_table(image_path, image_alt_text, image_url, description_eng, name_eng, url,
+                                            description_fin, name_fin, latitude, longitude, postal_code, street_name,
+                                            city, available_time, price)
+
+    @staticmethod
+    def insert_data_into_table(image_path, image_alt_text, image_url, description_eng, name_eng, url,
+                               description_fin, name_fin, latitude, longitude, postal_code, street_name, city,
+                               available_time, price):
+        Place.objects.create(image_path=image_path, image_alt_text=image_alt_text, image_url=image_url,
+                             description_eng=description_eng, name_eng=name_eng, url=url,
+                             description_fin=description_fin,
+                             name_fin=name_fin, latitude=latitude, longitude=longitude, postal_code=postal_code,
+                             street_name=street_name, city=city, available_time=available_time, price=price)
 
 
 def get_cities_and_places(city=None, month=None):
@@ -21,3 +180,25 @@ def get_cities_and_places(city=None, month=None):
              'available_time': item['available_time']})
     cities_and_places = dict(cities_and_places)
     return cities_and_places
+
+
+class WeatherAPI:
+    @staticmethod
+    def get_api_for_current_weather():
+        api_key = os.getenv('WEATHER_API_KEY')
+        return api_key
+
+    @staticmethod
+    def get_current_weather(lat, lon):
+        api_key = WeatherAPI.get_api_for_current_weather()
+        url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric'
+        try:
+            response = requests.get(url, timeout=3).json()
+            weather_parameter = response['weather'][0]['main'].lower()
+            temp = int(response['main']['temp'])
+            icon = response['weather'][0]['icon']
+            icon_path = f'http://openweathermap.org/img/w/{icon}.png'
+        except requests.exceptions.RequestException as e:
+            return {"error": str(e)}
+
+        return weather_parameter, temp, icon_path
--- places/views.py
+++ places/views.py
@@ -1,156 +1,8 @@
-import os
-import json
-
-from decimal import Decimal
-
-from dotenv import load_dotenv
-
-import requests
-from django.http import JsonResponse
 from django.views.generic import ListView, DetailView
 
 from .forms import PlaceSelectorForm
 from .models import Place
-from .services import get_cities_and_places
-
-load_dotenv()
-
-
-def get_access():
-    url = 'https://iam-datahub.visitfinland.com/auth/realms/Datahub/protocol/openid-connect/token'
-
-    access_data = {
-        'client_id': 'datahub-api',
-        'client_secret': f'{os.getenv("SECRET_KEY")}',
-        'grant_type': 'password',
-        'username': f'{os.getenv("USERNAME")}',
-        'password': f'{os.getenv("PASSWORD")}'
-    }
-
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-
-    try:
-        response = requests.post(url, data=access_data, headers=headers)
-        access_token = response.json().get('access_token')
-        return access_token
-    except Exception as e:
-        return JsonResponse(f"Error getting access token: {e}", status=400)
-
-
-def make_api_request(query):
-    request_url = 'https://api-datahub.visitfinland.com/graphql/v1/graphql'
-    access_token = get_access()
-
-    headers = {
-        'Authorization': f'Bearer {access_token}',
-        'Content-Type': 'application/json'
-    }
-
-    body = {
-        'query': query,
-    }
-
-    try:
-        response = requests.post(request_url, headers=headers, data=json.dumps(body))
-        response_data = response.json()
-        return response_data
-    except Exception as e:
-        return JsonResponse(f"Error getting access token: {e}", status=400)
-
-
-def save_data(query, filename):
-    with open(filename, 'w') as f:
-        data = make_api_request(query)
-        json.dump(data, f)
-
-
-def get_all_products_data():
-    query = """ {
-                product (where: {
-                    _and: [
-                        {type:{_eq:attraction}},
-                        {productTags:{tag:{_eq:"hiking_walking_trekking"}}},
-                        {productTargetGroups:{targetGroupId:{_eq:b2c}}},
-                    ]}){
-                    productImages {
-                        filename
-                        altText
-                        largeUrl
-                    }
-                    productInformations {
-                        description
-                        name
-                        url
-                    }
-                    postalAddresses {
-                        location
-                        postalCode
-                        streetName
-                        city
-                    }
-                    productAvailableMonths{
-                        month
-                    }
-                    productPricings {
-                        fromPrice
-                        toPrice
-                    }
-                }
-            }"""
-    save_data(query, 'data.json')
-
-
-# clear all data by searching for english and finnish data and deleting another languages data
-
-def get_data_from_file(filename):
-    with open(filename, 'r') as f:
-        data = json.load(f)
-        for product in data['product']:
-            # image info
-            image_path = product['productImages'][0]['filename']
-            image_alt_text = product['productImages'][0]['altText'].strip() if product['productImages'][0][
-                                                                                   'altText'] is not None else 'Product image'
-            image_url = product['productImages'][0]['largeUrl']
-
-            # product info in english
-            description_eng = product['productInformations'][0]['description']
-            name_eng = product['productInformations'][0]['name']
-            url = product['productInformations'][0]['url'] if product['productInformations'][0]['url'] else \
-                product['productInformations'][1]['url']
-
-            # product info in finnish
-            description_fin = product['productInformations'][1]['description']
-            name_fin = product['productInformations'][1]['name']
-
-            # location info
-            location = product['postalAddresses'][0]['location'][1:-1].split(',')
-            latitude = Decimal(location[0])
-            longitude = Decimal(location[1])
-            postal_code = product['postalAddresses'][0]['postalCode']
-            street_name = product['postalAddresses'][0]['streetName']
-            city = product['postalAddresses'][0]['city']
-
-            # time for visiting info
-            available_time = ', '.join([month['month'] for month in product['productAvailableMonths']])
-
-            # price info
-            price = 'Free' if (product['productPricings'][0]['fromPrice'] == 0.0 and product['productPricings'][0][
-                'toPrice'] == 0.0) else f'Paid (€)'
-
-            insert_data_into_table(image_path, image_alt_text, image_url, description_eng, name_eng, url,
-                                   description_fin, name_fin, latitude, longitude, postal_code, street_name, city,
-                                   available_time, price)
-
-
-def insert_data_into_table(image_path, image_alt_text, image_url, description_eng, name_eng, url,
-                           description_fin, name_fin, latitude, longitude, postal_code, street_name, city,
-                           available_time, price):
-    Place.objects.create(image_path=image_path, image_alt_text=image_alt_text, image_url=image_url,
-                         description_eng=description_eng, name_eng=name_eng, url=url, description_fin=description_fin,
-                         name_fin=name_fin, latitude=latitude, longitude=longitude, postal_code=postal_code,
-                         street_name=street_name, city=city, available_time=available_time, price=price)
+from .services import get_cities_and_places, WeatherAPI
 
 
 class PlaceView(ListView):
@@ -209,5 +61,10 @@ class HikingPlaceView(DetailView):
             last_month = best_time_to_visit[-1][:3].title()
             place['available_time'] = f'{first_month} - {last_month}'
 
-        context.update({'place': place})
+        weather_parameter, temp, icon_path = WeatherAPI.get_current_weather(place['latitude'], place['longitude'])
+
+        # description = [s.strip() for s in place['description_eng'].split('.') if s.strip()]
+        # place['description_eng'] = description
+
+        context.update({'place': place, 'weather_parameter': weather_parameter, 'temp': temp, 'icon_path': icon_path})
         return context
