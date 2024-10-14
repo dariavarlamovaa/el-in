74c6f0cc4985affe7e612b7662f538c609c1283f
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2024-10-14 21:53:50 +0300

    updated docker compose, updated city getter in the form, updated markup

--- places/forms.py
+++ places/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.db import ProgrammingError
+from django.db import connection
 
 from places.models import Place
 
@@ -9,12 +9,16 @@ class PlaceSelectorForm(forms.Form):
         ('january', 'January'), ('february', 'February'), ('march', 'March'),
         ('april', 'April'), ('may', 'May'), ('june', 'June'),
         ('july', 'July'), ('august', 'August'), ('september', 'September'),
-        ('october', 'October'), ('november', 'November'), ('december', 'December')
-    ]
-    try:
-        city = forms.ChoiceField(choices=[(city, city) for city in
-                                          Place.objects.values_list('city', flat=True).order_by('city').distinct()],
-                                 required=False)
-    except ProgrammingError:
-        city = []
+        ('october', 'October'), ('november', 'November'), ('december', 'December')]
+    model = Place
+    city = forms.ChoiceField(required=False)
     month = forms.ChoiceField(choices=months, required=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if 'places_place' in connection.introspection.table_names():
+            city_choices = [(city, city) for city in
+                            Place.objects.values_list('city', flat=True).order_by('city').distinct()]
+        else:
+            city_choices = ['No cities', 'No cities'],
+        self.fields['city'].choices = city_choices
