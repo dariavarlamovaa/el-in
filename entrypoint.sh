a31dec99dee83838d3cf59593ce10f1bd5090a64
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2024-10-22 22:23:19 +0300

    updated docker compose and entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,6 @@
 #!/bin/sh
 
-if [ "$POSTGRES_ENGINE" = "django.db.backends.postgresql" ]
-then
-    echo "Waiting for postgres..."
-
-    while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started"
-fi
-
 python manage.py migrate
 python manage.py collectstatic --no-input --clear
-
-echo "Container finnhike_web is running, inserting data..."
-python manage.py insert_data
 
 exec "$@"
