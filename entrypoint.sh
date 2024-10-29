04b556c59d9da38e3a0ceb9bdaf3ebaab696586e
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2024-10-29 10:53:02 +0200

    updated entrypoint script, updated nginx configuration, updated translations

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,12 @@
 python manage.py migrate
 python manage.py collectstatic --no-input --clear
 
+if [ "$DJANGO_SUPERUSER_USERNAME" ]
+then
+    python manage.py createsuperuser \
+        --noinput \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_EMAIL
+fi
+
 exec "$@"
