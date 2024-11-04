d5dcd2027f84dcb62714fc1891db7a74ff4476e5
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2024-11-04 09:50:58 +0200

    added script for lang menu

--- static/js/language.js
+++ static/js/language.js
@@ -2,12 +2,25 @@ const langMenu = document.querySelector('.lang-menu')
 const langDropdown = document.querySelector('.lang-dropdown')
 const selectedLanguage = document.querySelector('.selected_language')
 
-langDropdown.style.display = langDropdown.style.display || "none";
-selectedLanguage.style.borderBottomRightRadius = langDropdown.style.borderBottomRightRadius || "5px";
-selectedLanguage.style.borderBottomLeftRadius = langDropdown.style.borderBottomLeftRadius || "5px";
+function updateLangDropdownVisibility() {
+    if (window.innerWidth > 950) {
+        langDropdown.style.display = "none";
+        selectedLanguage.style.borderBottomRightRadius = '5px'
+        selectedLanguage.style.borderBottomLeftRadius = '5px'
+    } else {
+        langDropdown.style.display = "flex";
+        selectedLanguage.style.borderBottomRightRadius = 'unset'
+        selectedLanguage.style.borderBottomLeftRadius = 'unset'
+    }
+}
+
+window.addEventListener('resize', updateLangDropdownVisibility);
+updateLangDropdownVisibility();
 
 langMenu.addEventListener('click', function () {
-    langDropdown.style.display = (langDropdown.style.display === "none" || langDropdown.style.display === "") ? "block" : "none";
-    selectedLanguage.style.borderBottomRightRadius = (selectedLanguage.style.borderBottomRightRadius === "unset" || selectedLanguage.style.borderBottomRightRadius === "") ? "5px" : "unset";
-    selectedLanguage.style.borderBottomLeftRadius = (selectedLanguage.style.borderBottomLeftRadius === "unset" || selectedLanguage.style.borderBottomLeftRadius === "") ? "5px" : "unset";
+    if (window.innerWidth > 950) {
+        langDropdown.style.display = (langDropdown.style.display === "none" || langDropdown.style.display === "") ? "block" : "none";
+        selectedLanguage.style.borderBottomRightRadius = (selectedLanguage.style.borderBottomRightRadius === "unset" || selectedLanguage.style.borderBottomRightRadius === "") ? "5px" : "unset";
+        selectedLanguage.style.borderBottomLeftRadius = (selectedLanguage.style.borderBottomLeftRadius === "unset" || selectedLanguage.style.borderBottomLeftRadius === "") ? "5px" : "unset";
+    }
 });
